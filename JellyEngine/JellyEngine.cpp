fe75aa495304bdf7ab061473aed459c35e99a956
Rafagamedev <rpadiper@gmail.com>
2023-09-26 14:11:26 -0700

    Set up gitignore

---
+++ JellyEngine/JellyEngine.cpp
@@ -0,0 +1,12 @@
+﻿// JellyEngine.cpp : Defines the entry point for the application.
+//
+
+#include "JellyEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
