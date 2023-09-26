61e4a11c4f166e0d208a7a608a783444909cc318
Rafagamedev <rpadiper@gmail.com>
2023-09-26 14:39:42 -0700

    Added assimp library to project

--- JellyEngine/JellyEngine.cpp
+++ JellyEngine/JellyEngine.cpp
@@ -1,12 +1,10 @@
-﻿// JellyEngine.cpp : Defines the entry point for the application.
-//
-
-#include "JellyEngine.h"
+﻿#include "JellyEngine.h"
 
 using namespace std;
-
+// NOTE: When loading from resources, use RESOURCES_PATH
 int main()
 {
 	cout << "Hello JellyEngine." << endl;
+	cout << RESOURCES_PATH << endl;
 	return 0;
 }
--- JellyEngine/JellyEngine.h
+++ JellyEngine/JellyEngine.h
@@ -1,8 +1,4 @@
-﻿// JellyEngine.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
-
-// TODO: Reference additional headers your program requires here.
+#include <assimp/Importer.hpp>
