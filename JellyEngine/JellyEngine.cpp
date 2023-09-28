2561b7d8c78efa529aab15ed0f021df8ed6ab509
Rafagamedev <38381290+Rafapp@users.noreply.github.com>
2023-09-27 19:09:01 -0700

    Build Assimp library with CMAKE (#4)
    * Update README.md
    
    * CMAKE Project
    
    * Update README.md
    
    * Undo test
    
    * New gitignore, working on template
    
    * Set up gitignore
    
    * CMake setup complete, updated gitignore
    
    * Added assimp library to project

--- JellyEngine/JellyEngine.cpp
+++ JellyEngine/JellyEngine.cpp
@@ -1,12 +1,10 @@
-﻿// JellyEngine.cpp : Defines the entry point for the application.
-//
-
 #include "JellyEngine.h"
 
 using namespace std;
-
+// NOTE: When loading from resources, use RESOURCES_PATH
 int main()
 {
 	cout << "Hello JellyEngine." << endl;
-	return 0;
+	cout << RESOURCES_PATH << endl;
+  return 0;
 }
--- JellyEngine/JellyEngine.h
+++ JellyEngine/JellyEngine.h
@@ -1,8 +1,3 @@
-﻿// JellyEngine.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
+﻿#pragma once
 #include <iostream>
-
-// TODO: Reference additional headers your program requires here.
+#include <assimp/Importer.hpp>
