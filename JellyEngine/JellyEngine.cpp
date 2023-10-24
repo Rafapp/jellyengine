404fe3025f6b187869f99ac1eed27fe8ed516b19
Rafagamedev <rpadiper@gmail.com>
2023-10-24 07:47:29 -0700

    Hello Jelly Engine!

---
+++ JellyEngine/JellyEngine.cpp
@@ -0,0 +1,12 @@
+﻿// JellyEngine.cpp : Defines the entry point for the application.
+//
+
+#include "JellyEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Jelly Engine!" << endl;
+	return 0;
+}
