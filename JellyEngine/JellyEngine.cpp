148bbbb9e923ed52a4ef50babab2e1b05f24b67c
Rafagamedev <rpadiper@gmail.com>
2023-10-24 08:28:52 -0700

    Added GLFW

--- JellyEngine/JellyEngine.cpp
+++ JellyEngine/JellyEngine.cpp
@@ -2,11 +2,39 @@
 //
 
 #include "JellyEngine.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello Jelly Engine!" << endl;
-	return 0;
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Set GLFW to not create an OpenGL context
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Destroy the window and terminate GLFW
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
