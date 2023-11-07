9672b11b9b426516c80152a3a49d075a38fd8a8b
Rafagamedev <rpadiper@gmail.com>
2023-11-06 20:03:02 -0800

    Setting up openGL, we require the glad library

--- JellyEngine/JellyEngine.cpp
+++ JellyEngine/JellyEngine.cpp
@@ -1,30 +1,57 @@
-﻿// JellyEngine.cpp : Defines the entry point for the application.
-//
-
-#include "JellyEngine.h"
+﻿#include "JellyEngine.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 using namespace std;
 
+GLint SCREEN_WIDTH = 1280, SCREEN_HEIGHT = 720;
+
+// Callbacks a glfw error
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+// Checks for key input
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        cout << "ESC has been pressed." << endl;
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
 int main()
 {
+    GLFWwindow* window;
+
+    // Set error callback, in case we encounter any errors
+    glfwSetErrorCallback(error_callback);
+
     // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
+        glfwTerminate();
         return -1;
     }
 
-    // Set GLFW to not create an OpenGL context
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    // Require minimum OpenGL version 0
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
+    // Create a window
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Jelly Engine", nullptr, nullptr);
+
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
+    // Add our esc input to the glfw process
+    glfwSetKeyCallback(window, key_callback);
+
+    // Assign a openGL context (Assign memory in OS for openGL computations)
+    glfwMakeContextCurrent(window);
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
@@ -37,4 +64,6 @@ int main()
     glfwTerminate();
 
     return 0;
+
 }
+
