c39e6c4fcf552efea7445840deaeb1fd2536d016
Rafagamedev <38381290+Rafapp@users.noreply.github.com>
2023-11-06 21:25:16 -0800

    OpenGL triangle (#18)
    * Update README.md
    
    * CMAKE Project
    
    * Update README.md
    
    * Undo test
    
    * New gitignore, working on template
    
    * Set up gitignore
    
    * CMake setup complete, updated gitignore
    
    * Added assimp library to project
    
    * Fix cmake
    
    * Rafa (#6) (#7)
    
    Fix merge conflict
    
    * Jelly engine logo
    
    * Attempted glfw setup, ran into issue with dll
    
    the code execution cannot proceed because glfw3d.dll was not found
    
    * Removed assimp
    
    * Fresh start
    
    * Hello Jelly Engine!
    
    * Added GLFW
    
    * Example files
    
    * Setting up openGL, we require the glad library
    
    * Added GLAD
    
    * Add glm, add glad, spinning triangle
    
    * Made screen scale correctly
    
    * render loop lol

--- JellyEngine/JellyEngine.cpp
+++ JellyEngine/JellyEngine.cpp
@@ -1,40 +1,152 @@
-﻿// JellyEngine.cpp : Defines the entry point for the application.
-//
-
-#include "JellyEngine.h"
+﻿#include "JellyEngine.h"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
 using namespace std;
+
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
+
+// Callbacks a glfw error
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+// Checks for key input
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        cout << "ESC has been pressed." << endl;
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
 
 int main()
 {
-    // Initialize GLFW
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
+    GLint wWidth = 1280, wHeight = 720;
+
+    glfwSetErrorCallback(error_callback);
+
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
+        glfwTerminate();
         return -1;
     }
 
-    // Set GLFW to not create an OpenGL context
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
+    window = glfwCreateWindow(wWidth, wHeight, "Jelly Engine", nullptr, nullptr);
+
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window)) {
-        // Poll for and process events
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSwapInterval(1);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+        sizeof(vertices[0]), (void*)0);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+        sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        glm::mat4 model, projection, mvp;
+
+        glfwGetFramebufferSize(window, &wWidth, &wHeight);
+        ratio = wWidth / (float)wHeight;
+
+        glViewport(0, 0, wWidth, wHeight);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        projection = glm::ortho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
+        mvp = projection * model;
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Destroy the window and terminate GLFW
     glfwDestroyWindow(window);
-    glfwTerminate();
 
-    return 0;
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+
 }
