992eb1b51aa03967ceaafcbeabd0b38c9b8bd458
Rafagamedev <rpadiper@gmail.com>
2023-11-29 21:00:59 -0800

    Rotating plane on 3D space

--- JellyEngine/JellyEngine.cpp
+++ JellyEngine/JellyEngine.cpp
@@ -31,7 +31,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 int main()
 {
     /*
-     * SET UP GLFW, GLAD AND OPENGL
+     * SET UP GLFW, GLAD AND OPENGL: Todo ~ Make renderer class
      */
 
     cout << "JELLY ENGINE: VERSION 1.0.0" << endl;
@@ -78,7 +78,7 @@ int main()
     Shader shader(RESOURCES_PATH "shaders/shader.vert", RESOURCES_PATH "shaders/shader.frag");
 
     /*
-     * BUFFERS
+     * BUFFERS: Todo ~ Make mesh class
      */
 
      // Vertices to draw, we use the indexed triangle data structure
@@ -105,7 +105,6 @@ int main()
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-
     // Create an element buffer object (use the indexed triangle data structure)
     unsigned int EBO;
     glGenBuffers(1, &EBO);
@@ -124,17 +123,33 @@ int main()
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
 
     /*
-     * CAMERA 
+     * MODEL: Todo ~ Make a model class 
      */
-     
+    glm::mat4 model = glm::mat4(1.0f);
+    
+
+    /*
+     * CAMERA: Todo ~ Make camera class
+     */
+
+    // Camera attributes
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
     // View matrix
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // Camera's position
-
-    // Projection matrix
     glm::mat4 projection;
-    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)wWidth / (float)wHeight, 0.1f, 100.0f);
 
+    // LookAt matrix
+    glm::mat4 view;
+    view = glm::lookAt(cameraPos, cameraTarget, up);
+
+    // For fun, we can orbit the camera around!
+    const float orbitRadius = 10;
 
     // Render loop
     while (!glfwWindowShouldClose(window))
@@ -149,6 +164,21 @@ int main()
         // BG Color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Rotate camera
+        float camX = sin(glfwGetTime()) * orbitRadius;
+        float camZ = cos(glfwGetTime()) * orbitRadius;
+        cameraPos = glm::vec3(camX, 0.0, camZ);
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f) * 0.001f, glm::vec3(0.5f, 1.0f, 0.5f));
+        // Apply matrix transformations in shader
+        int modelLoc = glGetUniformLocation(shader.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        int viewLoc = glGetUniformLocation(shader.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projectionLoc = glGetUniformLocation(shader.ID, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         // Activate shader
         shader.use();
